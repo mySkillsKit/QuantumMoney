f6e7c87ad0cf44971f9d2074771276d8ebabdd42
Anatolii Vasilev <avas2012@gmail.com>
2022-08-06 21:14:39 +0400

    initialization

---
+++ src/main/java/ru/netology/quantummoney/QuantumMoneyApplication.java
@@ -0,0 +1,13 @@
+package ru.netology.quantummoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuantumMoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuantumMoneyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/netology/quantummoney/QuantumMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.quantummoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuantumMoneyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
